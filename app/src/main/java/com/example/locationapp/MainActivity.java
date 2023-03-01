b8746f0b5a49fee7ac6be5152366ce05fe3cd44f
PriyanthaFdo <kjpriyanthafdo@gmail.com>
2023-03-01 11:54:58 +0530

    only getLastLocation working on all sdk.

--- app/src/main/java/com/example/locationapp/MainActivity.java
+++ app/src/main/java/com/example/locationapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.locationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -12,6 +13,7 @@ import android.location.LocationRequest;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,8 +24,10 @@ import com.google.android.gms.tasks.CancellationTokenSource;
 
 public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient fusedLocationProviderClient;
-    CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-    CancellationToken cancellationToken = cancellationTokenSource.getToken();
+    CancellationTokenSource cancellationTokenSource;
+    CancellationToken cancellationToken;
+
+    boolean isRunning;
 
     Button btn_getLocation;
     TextView txt_locationResult;
@@ -36,9 +40,18 @@ public class MainActivity extends AppCompatActivity {
         btn_getLocation = findViewById(R.id.btn_getLocation);
         txt_locationResult = findViewById(R.id.txt_locationResult);
 
+        isRunning = false;
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        btn_getLocation.setOnClickListener(v -> getLocation());
+        btn_getLocation.setOnClickListener(v -> {
+            if(!isRunning) {
+                cancellationTokenSource = new CancellationTokenSource();
+                cancellationToken = cancellationTokenSource.getToken();
+                isRunning = true;
+                getLocation();
+            }
+        });
     }
 
     private void getLocation(){
@@ -57,29 +70,41 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        getLocation();
+    }
+
     @SuppressLint("MissingPermission")
     private void getCurrentLocation() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Log.d("getCurrentLocation", "new Sdk");
             fusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_BALANCED_POWER_ACCURACY, cancellationToken)
                     .addOnSuccessListener(this::displayLocation);
         } else {
+            Log.d("getCurrentLocation", "old Sdk");
             fusedLocationProviderClient.getLastLocation()
                     .addOnSuccessListener(this::displayLocation);
         }
     }
 
     private void displayLocation(Location location) {
+        cancellationTokenSource = new CancellationTokenSource();
         if(location != null){
             double longitude = location.getLongitude();
             double latitude = location.getLatitude();
 
-            txt_locationResult.setText(getResources().getString(R.string.location, latitude, longitude));
+            String currentText = txt_locationResult.getText().toString();
+            String newText = getResources().getString(R.string.location, latitude, longitude);
+            txt_locationResult.setText(getResources().getString(R.string.multiLine, newText, currentText));
             cancellationTokenSource.cancel();
 
-            new Handler().postDelayed(() -> txt_locationResult.setText(""), 3000);
         } else {
             txt_locationResult.setText(R.string.errorRetrievingLocation);
+            new Handler().postDelayed(() -> txt_locationResult.setText(""), 3000);
         }
+        isRunning = false;
     }
 }
 
